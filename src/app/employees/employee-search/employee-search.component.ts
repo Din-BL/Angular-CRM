00b6b4aea284c882a05855f76fb4dcd1ec50db91
Din <Dinz5005@gmail.com>
2023-04-17 17:37:23 +0300

    employee-search

--- src/app/employees/employee-search/employee-search.component.ts
+++ src/app/employees/employee-search/employee-search.component.ts
@@ -6,7 +6,9 @@ import { SessionService } from 'src/app/core/session.service';
   templateUrl: './employee-search.component.html'
 })
 export class EmployeeSearchComponent {
+
   constructor(private employee: SessionService) { }
+
   searchEmployee(event: Event) {
     const data = (<HTMLInputElement>event.target!).value;
     this.employee.searchEmployee.next(data)
