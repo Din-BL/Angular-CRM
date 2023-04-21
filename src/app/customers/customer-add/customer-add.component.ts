b91489440850acf2a550886e462f9343f77c3d4f
Din <Dinz5005@gmail.com>
2023-04-21 11:00:41 +0300

    switch-case-errors

--- src/app/customers/customer-add/customer-add.component.ts
+++ src/app/customers/customer-add/customer-add.component.ts
@@ -30,14 +30,55 @@ export class CustomerAddComponent implements OnInit {
     return this.addForm.customerForm.get(field) as FormControl;
   }
 
+  errorCase(msg?: string, field?: string, option?: boolean, length?: string): string {
+    if (msg) {
+      return `"${msg}" is not allowed to be empty`
+    } else {
+      if (option) {
+        return `${field} is required `;
+      }
+      return `${field} must be at least ${length} characters long `;
+    }
+  }
+
+  lengthError(field: string, length: string): string {
+    return `"${field}" length must be at least ${length} characters long`
+  }
+
   onSubmit() {
     this.customerApi.addCustomer(this.addForm.customerForm.value).subscribe({
-      next: data => {
+      next: (data: Person) => {
         this.users?.push(data),
           this.addForm.customerForm.reset()
       },
-      error: error => {
+      error: (error) => {
         this.errorMsg = error.error[0].message ? error.error[0].message : error.error
+        switch (this.errorMsg) {
+          case this.errorCase('first_name'):
+            this.errorMsg = this.errorCase(undefined, 'First name', true);
+            break;
+          case this.lengthError("first_name", "2"):
+            this.errorMsg = this.errorCase(undefined, 'First name', undefined, '2');
+            break;
+          case this.errorCase('last_name'):
+            this.errorMsg = this.errorCase(undefined, 'Last name', true);
+            break;
+          case this.lengthError("last_name", "2"):
+            this.errorMsg = this.errorCase(undefined, 'Last name', undefined, '2');
+            break;
+          case this.errorCase('phone'):
+            this.errorMsg = this.errorCase(undefined, 'Phone', true);
+            break;
+          case this.lengthError("phone", "9"):
+            this.errorMsg = this.errorCase(undefined, 'Phone', undefined, '9');
+            break;
+          case this.errorCase('email'):
+            this.errorMsg = this.errorCase(undefined, 'Email', true);
+            break;
+          case `"email" must be a valid email`:
+            this.errorMsg = 'Email is not valid'
+            break;
+        }
         Swal.fire({
           icon: 'error',
           title: this.errorMsg
